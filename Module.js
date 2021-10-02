bfd5cae714bedebc1c421045fe7d392915b8312d
wioniqle-q <69215407+wioniqle-q@users.noreply.github.com>
2021-10-02 15:01:53 +0300

    Update Module.js

--- Module.js
+++ Module.js
@@ -6,38 +6,50 @@ const dangerPerms = ["ADMINISTRATOR", "KICK_MEMBERS", "MANAGE_GUILD", "BAN_MEMBE
 const client = global.Client;
 
 async function roleBackup(guild) {
-  await RoleModel.deleteMany({});
-  guild.roles.cache.forEach(role => {
-    if(role.members.size <= 0) return;
-    RoleModel.updateOne({ Id: role.id }, { $set: { Members: role.members.map(member => member.id) } }, { upsert: true }).exec();
-  });
-  console.log("Role data is saved!");
+  try {
+    await RoleModel.deleteMany({}).exec();
+    guild.roles.cache.forEach((_role) => {
+      if (Object.keys(_role.members.size) === 0) return;
+      RoleModel.updateOne({ Id: role.id }, { $set: { Members: role.members.map(member => member.id) } }, { upsert: true }).exec();
+    });
+    console.log("Role data is saved!");
+  } catch ({}) {
+    Promise.reject(null);
+  };
 };
 
 async function channelBackup(guild) {
-  await ChannelModel.deleteMany({});
-  guild.channels.cache.forEach(async function (channel){
-    await new ChannelModel({
-      Id: channel.id,
-      Type: channel.type,
-      Parent: channel.parent ? channel.parentId: null,
-      Permissions: [...channel.permissionOverwrites.cache.values()].map(function(permissions){
-        return {
-          id: permissions.id,
-          type: permissions.type,
-          allow: new Permissions(permissions.allow.bitfield).toArray(),
-          deny: new Permissions(permissions.deny.bitfield).toArray()
-        }
-      })
-    }).save();
-  });
-  console.log("Channel data is saved!");
+  try {
+    await ChannelModel.deleteMany({}).exec();
+    guild.channels.cache.forEach(async (_channel) => {
+      await new ChannelModel({
+        Id: channel.id,
+        Type: channel.type,
+        Parent: channel.parent ? channel.parentId: null,
+        Permissions: [...channel.permissionOverwrites.cache.values()].map((_permissions) => {
+          return {
+            id: _permissions.id,
+            type: _permissions.type,
+            allow: new Permissions(_permissions.allow.bitfield).toArray(),
+            deny: new Permissions(_permissions.deny.bitfield).toArray()
+          };
+        })
+      }).save();
+    });
+    console.log("Channel data is saved!");
+  } catch ({}) {
+    Promise.reject(null);
+  };
 };
 
 async function closeAllPermissions(Guild) {
-  Guild.roles.cache.filter((role) => role.editable && dangerPerms.some((x) => role.permissions.has(x))).forEach(async (require) => {
-    await require.setPermissions(0n);
-  });
+  try {
+    Guild.roles.cache.filter((role) => role.editable && dangerPerms.some((x) => role.permissions.has(x))).forEach(async (equal) => {
+      await equal.setPermissions(0n);
+    });
+  } catch ({}) {
+    Promise.reject(null);
+  };
 };
 
 module.exports = {
