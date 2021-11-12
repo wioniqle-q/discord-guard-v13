9b5b33f29b4a1ff54755e24ec0f7de684b7e350c
wioniqle-q <69215407+wioniqle-q@users.noreply.github.com>
2021-11-12 13:33:32 +0300

    Create Models.js

---
+++ src/Models.js
@@ -0,0 +1,26 @@
+const Mongoose = require("mongoose");
+
+const RoleSchema = new Mongoose.Schema({
+  Id: String,
+  Members: { type: Array, default: [] }
+});
+
+const ChannelSchema = new Mongoose.Schema({
+  Id: { type: String, default: null },
+  Name: { type: String, default: null },
+  Type: { type: String, default: null },
+  Topic: { type: String, default : null },
+  Nsfw: { type: Boolean, default: false },
+  Bitrate: { type: Number, default: 64 },
+  UserLimit: { type: Number, min: 0, max: 99 },
+  Permissions: { type: Array, default: [] },
+  Position: Number,
+  Parent: { type: String, default: null },
+  RateLimitPerUser: { type: Number, default: 0 },
+  RtcRegion: { type: String, default: "europe" }
+});
+
+module.exports = {
+  RoleModel: Mongoose.model("Roles", RoleSchema),
+  ChannelModel: Mongoose.model("Channels", ChannelSchema)
+};
