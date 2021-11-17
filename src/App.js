abdc6988e354d955e806bbfa1fa0035cc9ffbce2
wioniqle-q <69215407+wioniqle-q@users.noreply.github.com>
2021-11-17 08:53:44 +0300

    A big update / you look this

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ mongoose.connection.on("connected", () => {
 for (const Token of Config.BOTS.TOKENS) {
   const Bot = new Client({ intents: [32767] });
   Bot.once("ready", () => {
-    Bot.Sleep = false, Bot.Thread = 0;
+    Bot.Sleep = false;
     Guards.push(Bot);
   });  
   Bot.login(Token);
@@ -42,73 +42,91 @@ Guard.login(Config.BOTS.MAIN_TOKEN);
  */
 Guild.prototype.find_entry = async function(type) {
   const audit = await this.fetchAuditLogs({ limit: 1, type: type }).then((audit) => audit.entries.first());
-  if (!audit || Date.now() - audit.createdTimestamp >= 10000 || safeMembers(audit.executor.id)) return true;
+  if (!audit || Date.now() - audit.createdTimestamp > 5000 || safeMembers(audit.executor.id)) return true;
   
   let guild = createIndex(1)[0].guilds.cache.get(Config.SERVER.GUILD_ID); 
   guild.members.kick(audit.executor.id).catch(() => false);
 };
 
 Guard.on("channelDelete", async (channel) => {
-  if (await channel.guild.find_entry("CHANNEL_DELETE", true) === true) return;
-  Danger = true;
+  const audit = await channel.guild.fetchAuditLogs({ limit: 5, type: "CHANNEL_DELETE" }).then((audit) => audit.entries.first());
+  if (!audit || safeMembers(audit.executor.id)) return;
   
-  let document = await ChannelModel.findOne({ Id: channel.id }).lean();
-  if (!document) return;
-  
-  await channel.clone({
-    name: document.Name,
-    nsfw: document.Nsfw,
-    type: document.Type,
-    topic: document.Topic,
-    position: document.Position,
-    permissionOverwrites: document.Permissions,
-    userLimit: document.UserLimit,
-    rateLimitPerUser: document.RateLimitPerUser,
-    rtcRegion: document.RtcRegion
-  }).then(async (newChannel) => {
-    await ChannelModel.updateOne({ Id: channel.id }, { $set: { Id: newChannel.id } }).exec();
-    await RoleModel.updateMany({ "Permissions.$.id": channel.id }, { $set: { "Permissions.$.id": newChannel.id } }).exec();
-  
-    if (channel.parentId) await newChannel.setParent(channel.parentId);
+  if (audit && (new Date().getTime() - new Date((audit.id / 4194304) + 1420070400000).getTime()) < 3000) {
+    Danger = true;
     
-    if (channel.type === "GUILD_CATEGORY") {
-      let documents = await ChannelModel.find({ Parent: channel.id }).lean();
-      if (!documents) return;
+    let guild = createIndex(1)[0].guilds.cache.get(Config.SERVER.GUILD_ID); 
+    guild.members.kick(audit.executor.id).catch(() => false);
     
-      await ChannelModel.updateMany({ Parent: channel.id }, { $set: { Parent: newChannel.id } }).exec();
-      let guild = createIndex(1)[0].guilds.cache.get(Config.SERVER.GUILD_ID); 
+    let document = await ChannelModel.findOne({ Id: channel.id }).lean();
+    if (!document) return;
+    
+    await channel.clone({
+      name: document.Name,
+      nsfw: document.Nsfw,
+      type: document.Type,
+      topic: document.Topic,
+      position: document.Position,
+      permissionOverwrites: document.Permissions,
+      userLimit: document.UserLimit,
+      rateLimitPerUser: document.RateLimitPerUser,
+      rtcRegion: document.RtcRegion
+    }).then(async (newChannel) => {
+      await ChannelModel.updateOne({ Id: channel.id }, { $set: { Id: newChannel.id } }).exec();
+      await RoleModel.updateMany({ "Permissions.$.id": channel.id }, { $set: { "Permissions.$.id": newChannel.id } }).exec();
+  
+      if (channel.parentId) await newChannel.setParent(channel.parentId);
+    
+      if (channel.type === "GUILD_CATEGORY") {
+        let documents = await ChannelModel.find({ Parent: channel.id }).lean();
+        if (!documents) return;
+    
+        await ChannelModel.updateMany({ Parent: channel.id }, { $set: { Parent: newChannel.id } }).exec();
+        let guild = createIndex(1)[0].guilds.cache.get(Config.SERVER.GUILD_ID); 
       
-      await chillout.forOf(documents, async Id => {
-        const parent = guild.channels.cache.get(Id.Id);
-        await parent?.setParent(newChannel.id, { lockPermissions: false });
-      }).then(() => chillout.StopIteration);
-    };
-  });
+        await chillout.forEach(documents, async Id => {
+          const parent = guild.channels.cache.get(Id.Id);
+          await parent?.setParent(newChannel.id, { lockPermissions: false });
+        }).then(() => chillout.StopIteration);
+      };
+    });
+  };
 });
 
 Guard.on("channelCreate", async (channel) => {
-  if (await channel.guild.find_entry("CHANNEL_CREATE", true)) return;
+  const audit = await channel.guild.fetchAuditLogs({ limit: 5, type: "CHANNEL_CREATE" }).then((audit) => audit.entries.first());
+  if (!audit || new Date().getTime() - new Date((audit.id / 4194304) + 1420070400000).getTime() > 3000 || safeMembers(audit.executor.id)) return;
+  
+  let guild = createIndex(1)[0].guilds.cache.get(Config.SERVER.GUILD_ID); 
+  guild.members.kick(audit.executor.id).catch(() => false);
+  
   channel.deleted ? true : await channel.delete();
 });
 
 Guard.on("channelUpdate", async (oldChannel, newChannel) => {
-  if (await oldChannel.guild.find_entry("CHANNEL_UPDATE", true)) return;
+  const audit = await oldChannel.guild.fetchAuditLogs({ limit: 5, type: "CHANNEL_UPDATE" }).then((audit) => audit.entries.first());
+  if (!audit || safeMembers(audit.executor.id)) return;
   
-  const document = await ChannelModel.findOne({ Id: oldChannel.id }).lean();
-  if (!document) return;
+  if (audit && (new Date().getTime() - new Date((audit.id / 4194304) + 1420070400000).getTime()) < 3000) {
+    const document = await ChannelModel.findOne({ Id: oldChannel.id }).lean();
+    if (!document) return;
   
-  await newChannel.edit({
-    name: document.name,
-    nsfw: document.Nsfw,
-    parent: document.Parent,
-    type: document.Type,
-    topic: document.Topic,
-    position: document.rawPosition,
-    permissionOverwrites: document.Permissions,
-    userLimit: document.UserLimit,
-    rateLimitPerUser: document.RateLimitPerUser,
-    rtcRegion: document.RtcRegion
-  });
+    await newChannel.edit({
+      name: oldChannel.name,
+      type: oldChannel.type,
+      position: oldChannel.rawPosition,
+      topic: oldChannel.topic,
+      nsfw: oldChannel.nsfw,
+      bitrate: oldChannel.bitrate,
+      userLimit: oldChannel.userLimit,
+      parent: oldChannel.parent,
+      lockPermissions: oldChannel.lockPermissions,
+      permissionOverwrites: document.Permissions,
+      rateLimitPerUser: oldChannel.rateLimitPerUser,
+      defaultAutoArchiveDuration: oldChannel.defaultAutoArchiveDuration,
+      rtcRegion: oldChannel.rtcRegion
+    });
+  };
 });
 
 Guard.on("channelUpdate", async (oldChannel, newChannel) => {
@@ -121,63 +139,83 @@ Guard.on("channelUpdate", async (oldChannel, newChannel) => {
 });
 
 Guard.on("roleDelete", async (role) => {
-  if (await role.guild.find_entry("ROLE_DELETE", true) === true) return;
-  Danger = true;
+  const audit = await role.guild.fetchAuditLogs({ limit: 5, type: "ROLE_DELETE" }).then((audit) => audit.entries.first());
+  if (!audit || safeMembers(audit.executor.id)) return;
   
-  let newRole = await role.guild.roles.create({
-    name: role.name,
-    color: role.color,
-    hoist: role.hoist,
-    permissions: role.permissions,
-    position: role.rawPosition,
-    mentionable: role.mentionable,
-    icon: role.icon,
-    unicodeEmoji: role.unicodeEmoji
-  });
+  if (audit && (new Date().getTime() - new Date((audit.id / 4194304) + 1420070400000).getTime()) < 3000) {
+    Danger = true;
+    
+    let guild = createIndex(1)[0].guilds.cache.get(Config.SERVER.GUILD_ID); 
+    guild.members.kick(audit.executor.id).catch(() => false);
+    
+    let newRole = await role.guild.roles.create({
+      name: role.name,
+      color: role.color,
+      hoist: role.hoist,
+      permissions: role.permissions,
+      position: role.rawPosition,
+      mentionable: role.mentionable,
+      icon: role.icon,
+      unicodeEmoji: role.unicodeEmoji
+    });
 
-  const updatedRoles = await RoleModel.findOneAndUpdate({ Id: role.id }, { Id: newRole.id });
-  await ChannelModel.updateMany({ "Permissions.id": role.id }, { $set: { "Permissions.$.id": newRole.id } });
+    const updatedRoles = await RoleModel.findOneAndUpdate({ Id: role.id }, { $set: { Id: newRole.id} }).exec();
+    await ChannelModel.updateMany({ "Permissions.id": role.id }, { $set: { "Permissions.$.id": newRole.id } }).exec();
   
-  const updatedChannels = await ChannelModel.find({ "Permissions.id": newRole.id }).lean();
-  if (!updatedChannels || newRole.deleted) return false;
+    const updatedChannels = await ChannelModel.find({ "Permissions.id": newRole.id }).lean();
+    if (!updatedChannels || newRole.deleted) return false;
   
-  await chillout.forOf(updatedChannels, async overwrite => {
-    const guild = await createIndex(1)[0].guilds.cache.get(Config.SERVER.GUILD_ID);
-    const dChannel = await guild.channels.cache.get(overwrite.Id);
-    if (dChannel) await dChannel.edit({ permissionOverwrites: overwrite.Permissions });
-  }).then(() => chillout.StopIteration);
+    const guilds = await createIndex(1)[0].guilds.cache.get(Config.SERVER.GUILD_ID);
   
-  const arrayMembers = updatedRoles.Members;
-  const extraMembers = arrayMembers.length % Guards.length;
-  const perMembers = Math.round((arrayMembers.length - extraMembers) / Guards.length);
-  
-  for (let i=0, n = Guards.length; i < n; ++i){
-    const members = arrayMembers.splice(0, i === 0 ? perMembers + extraMembers : perMembers);
-    if (members.length <= 0) return false;
-  
-    const guild = Guards[i].guilds.cache.get(Config.SERVER.GUILD_ID);
-    await chillout.forEach(members, async Id => {
-      const member = await guild.members.cache.get(Id);
-      await member?.roles.add(newRole.id);
+    await chillout.forEach(updatedChannels, async overwrite => {
+      const dChannel = await guilds.channels.cache.get(overwrite.Id);
+      await dChannel?.edit({ permissionOverwrites: overwrite.Permissions });
     }).then(() => chillout.StopIteration);
+  
+    const arrayMembers = updatedRoles.Members;
+    const extraMembers = arrayMembers.length % Guards.length;
+    const perMembers = Math.round((arrayMembers.length - extraMembers) / Guards.length);
+  
+    await chillout.repeat(Guards.length, async i => {
+      const members = arrayMembers.splice(0, i === 0 ? perMembers + extraMembers : perMembers);
+      if (members.length <= 0) return false;
+    
+      const guild = Guards[i].guilds.cache.get(Config.SERVER.GUILD_ID);
+      await chillout.forEach(members, async Id => {
+        const member = await guild.members.cache.get(Id);
+        await member?.roles.add(newRole.id);
+      }).then(() => chillout.StopIteration);
+    });
   };
 });
 
 Guard.on("roleCreate", async (role) => {
-  if (await role.guild.find_entry("ROLE_CREATE", true)) return;
-  role.deleted ? true : await role.delete();
+  const audit = await role.guild.fetchAuditLogs({ limit: 5, type: "ROLE_CREATE" }).then((audit) => audit.entries.first());
+  if (!audit || safeMembers(audit.executor.id)) return;
+  
+  if (audit && (new Date().getTime() - new Date((audit.id / 4194304) + 1420070400000).getTime()) < 3000) {
+    Danger = true;
+    
+    let guild = createIndex(1)[0].guilds.cache.get(Config.SERVER.GUILD_ID); 
+    guild.members.kick(audit.executor.id).catch(() => false);
+    
+    role.deleted ? true : await role.delete(); 
+  };
 });
 
 Guard.on("roleUpdate", async (oldRole, newRole) => {
-  if (await oldRole.guild.find_entry("ROLE_UPDATE", true)) return;
+  const audit = await oldRole.guild.fetchAuditLogs({ limit: 1, type: "ROLE_UPDATE" }).then((audit) => audit.entries.first());
+  if (!audit || new Date().getTime() - new Date((audit.id / 4194304) + 1420070400000).getTime() > 3000 || safeMembers(audit.executor.id)) return;
   
-  const document = await RoleModel.findOne({ Id: oldRole.id }).lean();
-  if (!document) return;
+  let guild = createIndex(1)[0].guilds.cache.get(Config.SERVER.GUILD_ID); 
+  guild.members.kick(audit.executor.id).catch(() => false);
   
-  await newRole.edit({
+  let bot = createIndex(1)[0].guilds.cache.get(Config.SERVER.GUILD_ID);
+  await bot.roles.edit(newRole.id, {
     name: oldRole.name,
     color: oldRole.color,
     hoist: oldRole.hoist,
+    position: oldRole.rawPosition,
     permissions: oldRole.permissions,
     mentionable: oldRole.mentionable,
     icon: oldRole.icon,
@@ -193,7 +231,12 @@ Guard.on("guildUpdate", async (oldGuild, newGuild) => {
 });
 
 Guard.on("guildBanAdd", async (member) => {
-  if (await member.guild.find_entry("MEMBER_BAN_ADD", true)) return;
+  const audit = await member.guild.fetchAuditLogs({ limit: 1, type: "MEMBER_BAN_ADD" }).then((audit) => audit.entries.first());
+  if (!audit || new Date().getTime() - new Date((audit.id / 4194304) + 1420070400000).getTime() > 3000 || safeMembers(audit.executor.id)) return;
+  
+  let guild = createIndex(1)[0].guilds.cache.get(Config.SERVER.GUILD_ID); 
+  guild.members.kick(audit.executor.id).catch(() => false);
+
   await member.guild.bans.remove(member.user.id).catch(() => false);
 });
 
@@ -219,7 +262,7 @@ Guard.on("emojiUpdate", async (oldEmoji, newEmoji) => {
 
 Guard.on("emojiDelete", async (emoji) => {
   if (await emoji.guild.find_entry("EMOJI_DELETE", true)) return;
-  await emoji.guild.emojis.create(emoji.url, emoji.names);
+  await emoji.guild.emojis.create(emoji.url, emoji.name);
 });
 
 Guard.on("webhookUpdate", async (channel) => {
