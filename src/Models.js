2f34e18cb934149430290080c419ab10a9492ad2
wioniqle-q <69215407+wioniqle-q@users.noreply.github.com>
2021-11-30 20:33:09 +0300

    Update Models.js

--- src/Models.js
+++ src/Models.js
@@ -1,23 +1,23 @@
 const Mongoose = require("mongoose");
 
 const RoleSchema = new Mongoose.Schema({
-  Id: String,
-  Members: { type: Array, default: [] }
+  Id: { type: String, unique: true },
+  Members: { type: Array, default: [], unique: true }
 });
 
 const ChannelSchema = new Mongoose.Schema({
-  Id: { type: String, default: null },
-  Name: { type: String, default: null },
-  Type: { type: String, default: null },
-  Topic: { type: String, default : null },
-  Nsfw: { type: Boolean, default: false },
-  Bitrate: { type: Number, default: 64 },
-  UserLimit: { type: Number, min: 0, max: 99 },
-  Permissions: { type: Array, default: [] },
-  Position: Number,
-  Parent: { type: String, default: null },
-  RateLimitPerUser: { type: Number, default: 0 },
-  RtcRegion: { type: String, default: "russia" }
+  Id: { type: String, default: null, unique: true },
+  Name: { type: String, default: null, unique: true },
+  Type: { type: String, default: null, unique: true },
+  Topic: { type: String, default : null, unique: true },
+  Nsfw: { type: Boolean, default: false, unique: true },
+  Bitrate: { type: Number, default: 64, unique: true },
+  UserLimit: { type: Number, min: 0, max: 99, unique: true },
+  Permissions: { type: Array, default: [], unique: true },
+  Position: { type: Number, unique: true },
+  Parent: { type: String, default: null, unique: true },
+  RateLimitPerUser: { type: Number, default: 0, unique: true },
+  RtcRegion: { type: String, default: "russia", unique: true }
 });
 
 module.exports = {
