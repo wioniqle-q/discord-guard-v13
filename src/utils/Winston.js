8642388abd37b713021872a22491aff2cf15fc9c
wioniqle-q <69215407+wioniqle-q@users.noreply.github.com>
2022-04-07 23:34:23 +0300

    Simple optimization

--- src/utils/Winston.js
+++ src/utils/Winston.js
@@ -1,26 +1,23 @@
-const winston = require("winston");
-
-const logConfiguration = {
-  transports: [new winston.transports.Console()],
-  format: winston.format.combine(
-    winston.format.timestamp({
-      format: "MMM-DD-YYYY HH:mm:ss",
-    }),
-    winston.format.printf((info) => `${info.level}: ${info.message}`)
-  ),
+var $jscomp = $jscomp || {};
+$jscomp.scope = {};
+$jscomp.createTemplateTagFirstArg = function(a) {
+  return a.raw = a;
 };
-const logger = winston.createLogger(logConfiguration);
-
-module.exports = class Winston {
-  info(object) {
-    return logger.info(object);
-  }
-
-  warn(object) {
-    return logger.warn(object);
-  }
-
-  error(object) {
-    return logger.error(object);
-  }
+$jscomp.createTemplateTagFirstArgWithRaw = function(a, b) {
+  a.raw = b;
+  return a;
 };
+var winston = require("winston"), logConfiguration = {transports:[new winston.transports.Console()], format:winston.format.combine(winston.format.timestamp({format:"MMM-DD-YYYY HH:mm:ss",}), winston.format.printf(function(a) {
+  return a.level + ": " + a.message;
+})),}, logger = winston.createLogger(logConfiguration), Input_0$classdecl$var0 = function() {
+};
+Input_0$classdecl$var0.prototype.info = function(a) {
+  return logger.info(a);
+};
+Input_0$classdecl$var0.prototype.warn = function(a) {
+  return logger.warn(a);
+};
+Input_0$classdecl$var0.prototype.error = function(a) {
+  return logger.error(a);
+};
+module.exports = Input_0$classdecl$var0;
