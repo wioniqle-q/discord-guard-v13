b290c44fc62f7a1c983a9fc9bf21433164471aff
wioniqle-q <69215407+wioniqle-q@users.noreply.github.com>
2022-04-17 21:10:25 +0300

    build(api-type v10): updated API-Type v10

--- src/utils/Resolvers.js
+++ src/utils/Resolvers.js
@@ -1,20 +1,28 @@
-const { ChannelType } = require("discord-api-types/v9");
-
-module.exports = class Resolvers {
-  resolveChannelType(key) {
-    switch (key) {
-      case "GUILD_TEXT":
-        return ChannelType.GuildText;
-      case "GUILD_VOICE":
-        return ChannelType.GuildVoice;
-      case "GUILD_CATEGORY":
-        return ChannelType.GuildCategory;
-      default:
-        return unknownKeyStrategy(key);
-    }
+var $jscomp = $jscomp || {};
+$jscomp.scope = {};
+$jscomp.createTemplateTagFirstArg = function(a) {
+  return a.raw = a;
+};
+$jscomp.createTemplateTagFirstArgWithRaw = function(a, b) {
+  a.raw = b;
+  return a;
+};
+var $jscomp$destructuring$var0 = require("discord-api-types/v10"), ChannelType = $jscomp$destructuring$var0.ChannelType, Input_0$classdecl$var0 = function() {
+};
+Input_0$classdecl$var0.prototype.resolveChannelType = function(a) {
+  switch(a) {
+    case "GUILD_TEXT":
+      return ChannelType.GuildText;
+    case "GUILD_VOICE":
+      return ChannelType.GuildVoice;
+    case "GUILD_CATEGORY":
+      return ChannelType.GuildCategory;
+    default:
+      return unknownKeyStrategy(a);
   }
+};
+module.exports = Input_0$classdecl$var0;
+function unknownKeyStrategy(a) {
+  throw Error("Could not resolve enum value for " + a);
 }
-
-function unknownKeyStrategy(val) {
-  throw new Error(`Could not resolve enum value for ${val}`);
-}
+;
